04893ad3bf343d9b198e8566e06d3be0783c3c51
Praveen Korni <kornipraveen78@gmail.com>
2020-09-23 20:39:38 +0530

    Updates

---
+++ precache-manifest.643b01378495330f34d6e244da3303b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04204c0740a1d516d7d4955ee064942",
+    "url": "/MERN-learning-project-/index.html"
+  },
+  {
+    "revision": "45c4cbe640eff421c0d7",
+    "url": "/MERN-learning-project-/static/css/2.c6bf2efa.chunk.css"
+  },
+  {
+    "revision": "45c4cbe640eff421c0d7",
+    "url": "/MERN-learning-project-/static/js/2.bb04f0a8.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/MERN-learning-project-/static/js/2.bb04f0a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f835631cfb3c45ba50",
+    "url": "/MERN-learning-project-/static/js/main.ea1a9dbd.chunk.js"
+  },
+  {
+    "revision": "72913c3d0c9650bb823b",
+    "url": "/MERN-learning-project-/static/js/runtime-main.10171024.js"
+  }
+]);
